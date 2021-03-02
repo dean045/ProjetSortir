cd0294025c13dbc2a4f25a89d2f9c5ccaf0d64c0
gmanchelin <gmanchelin@gmail.com>
2021-03-02 15:49:09 +0100

    Bootstrap + if pour empêcher l'inscription à une sortie complète + asserts pour unicité (sur l'ajout de villes et sites)

--- src/Entity/Site.php
+++ src/Entity/Site.php
@@ -6,9 +6,14 @@ use App\Repository\SiteRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=SiteRepository::class)
+ * @UniqueEntity("nom", message="Ce site existe déjà!")
  */
 class Site
 {
--- src/Entity/Ville.php
+++ src/Entity/Ville.php
@@ -7,9 +7,13 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=VilleRepository::class)
+ * @UniqueEntity("nom", "codePostal", message="Ces informations existent déjà!")
  */
 class Ville
 {
