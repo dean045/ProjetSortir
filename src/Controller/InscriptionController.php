b3b9ecd3abb091f287eb709dd4a3193eeeb3ce3d
Brahim <Brahim.hajjibaba2020@campus-eni.fr>
2021-03-02 09:20:02 +0100

    csv ajout testé aprouvé

--- src/Controller/InscriptionController.php
+++ src/Controller/InscriptionController.php
@@ -4,13 +4,17 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\InscriptionUserType;
+use App\Form\UploadType;
+use App\Service\FileUploader;
 use Doctrine\ORM\EntityManagerInterface;
+use League\Csv\Reader;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class InscriptionController extends AbstractController
 {
@@ -67,7 +71,7 @@ class InscriptionController extends AbstractController
      * @param Request $request
      * @return Response
      */
-    public function upload(EntityManagerInterface $em, Request $request, FileUploader $fileUploader): Response
+    public function upload(EntityManagerInterface $em, Request $request,ValidatorInterface $validator, FileUploader $fileUploader, UserPasswordEncoderInterface $passwordEncoder): Response
     {
         $form = $this->createForm(UploadType::class);
         $form->handleRequest($request);
@@ -75,16 +79,47 @@ class InscriptionController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $file = $form->get("import")->getData();
             $file = $fileUploader->upload($file);
-            dd($file);
+            $ligne = 0;
 
-            $csv = Reader::createFromPath($file->getClientOriginalName())
+
+            $csv = Reader::createFromPath($_SERVER['DOCUMENT_ROOT']."\uploads\images/".$file)
                 ->setHeaderOffset(0);
 
-            $rowNo = 1;
+            foreach ($csv as $record) {
+                $user = new User();
+                $user->setUsername($record['username']);
+                $user->setPlainPassword($record['password']);
+                $user->setNom($record['nom']);
+                $user->setPrenom($record['prenom']);
+                $user->setTelephone($record['telephone']);
+                $user->setMail($record['mail']);
+                $user->setActif(true);
+                $user->setAdmin(false);
+                $user->setImage("aucune");
+                $site = $em->getRepository('App:Site')->findOneBy(['nom'=>$record['site']]);
+                $user->setSite($site);
+                $user->setPassword(
+                    $passwordEncoder->encodePassword(
+                        $user,
+                        $user->getPlainPassword()
+                    )
+                );
+                $errors = $validator->validate($user);
 
+                if (count($errors) == 0) {
+                    $em = $this->getDoctrine()->getManager();
+                    $em->persist($user);
+                    $em->flush();
+                }
+                else{
+                    $this->addFlash('warning', "L'ajout d'utilisateur s'est arrété à la ligne ".$ligne." car les données saisies ne sont pas valides.");
+                }
+                $ligne++;
+            }
+            $this->addFlash('success', $ligne." utilisateurs ont été inscrit.");
             return $this->redirectToRoute('liste');
         }
 
-        return $this->render('inscription/index.html.twig', ['form' => $form->createView(),]);
+        return $this->render('inscription/upload.html.twig', ['form' => $form->createView(),]);
     }
 }
