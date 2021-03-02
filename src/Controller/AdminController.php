cd32e9a232b8955a351a4042425953e65248ab36
Brahim <Brahim.hajjibaba2020@campus-eni.fr>
2021-03-02 14:29:18 +0100

    activer / desactiver user

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/admin", name="admin", methods={"GET"})
+     */
+    public function index(Request $request, EntityManagerInterface $em): Response
+    {
+        $liste = $em->getRepository(User::class)->findAll();
+
+
+        return $this->render('admin/index.html.twig', [
+            'liste' => $liste,
+        ]);
+    }
+
+
+    /**
+     * @Route("/admin_activer", name="admin_activer", methods={"GET","POST"})
+     */
+    public function activer(Request $request, EntityManagerInterface $em): Response
+    {
+        if ($request->isXmlHttpRequest()) {
+            $liste = $request->request->get('liste');
+            $compteur = 0;
+
+            foreach ($liste as $id)
+            {
+                $user = $em->getRepository(User::class)->find($id);
+                $user ->setActif(true);
+                $em->flush();
+                $compteur++;
+            }
+            return new JsonResponse($compteur, 200, [], true);
+        }
+    }
+
+    /**
+     * @Route("/admin_desactiver", name="admin_desactiver", methods={"GET","POST"})
+     */
+    public function desactiver(Request $request, EntityManagerInterface $em): Response
+    {
+        if ($request->isXmlHttpRequest()) {
+            $liste = $request->request->get('liste');
+            $compteur = 0;
+
+            foreach ($liste as $id)
+            {
+                $user = $em->getRepository(User::class)->find($id);
+                $user ->setActif(false);
+                $em->flush();
+                $compteur++;
+            }
+            return new JsonResponse($compteur, 200, [], true);
+        }
+    }
+}
